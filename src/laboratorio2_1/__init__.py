84f303bc53d9d58d11f1eadad56068e07d7cdd3f
MarcosMarquezOvalle <mmarquez17@gmail.com>
2026-08-11 16:33:10 -0600

    Setup pre-commit hooks and update example.txt

--- src/laboratorio2_1/__init__.py
+++ src/laboratorio2_1/__init__.py
@@ -1,28 +1,31 @@
+from __future__ import annotations
+
 import json
-from typing import Any
 
 try:
-    with open("example.txt", "r") as file:
+    with open('example.txt') as file:
         json_string = file.read()
         print(json_string)
         data = json.loads(json_string)
 
-    print(data["name"])         # Output: Alice
-    print(data["age"])          # Output: 30
-    print(data["is_employee"])  # Output: True
-    data["adress"] = "123 Main St"
+    print(data['name'])         # Output: Alice
+    print(data['age'])          # Output: 30
+    print(data['is_employee'])  # Output: True
+    data['adress'] = '123 Main St'
     json_string_updated = json.dumps(data)
-    print(json_string_updated)  # Output: {"name": "Alice", "age": 30, "is_employee": true, "adress": "123 Main St"}
+
+    print(json_string_updated)
     data = json.loads(json_string_updated)
-    data.pop("is_employee", None)  # Remove the "is_employee" key if it exists
+    data.pop('is_employee', None)  # Remove the "is_employee" key if it exists
     try:
-        del data["is_employee"]  # Remove the "is_employee" key if it exists
+        del data['is_employee']  # Remove the "is_employee" key if it exists
     except KeyError:
         print("Error: Key 'is_employee' not found in the dictionary.")
-    print(data)  # Output: {"name": "Alice", "age": 30, "adress": "123 Main St"}
+    # Output: {"name": "Alice", "age": 30, "adress": "123 Main St"}
+    print(data)
 except FileNotFoundError:
-    print("Error: The file does not exist.")
+    print('Error: The file does not exist.')
 except PermissionError:
-    print("Error: You do not have permission to read this file.")
+    print('Error: You do not have permission to read this file.')
 except Exception as e:
     print(f"An unexpected error occurred: {e}")
--- tests/test_parse_json.py
+++
@@ -1,8 +0,0 @@
-from laboratorio2_1 import parse_json_string
-
-
-def test_parse_json_string_returns_dict():
-    json_text = '{"name": "Alice", "age": 30}'
-    result = parse_json_string(json_text)
-
-    assert result == {"name": "Alice", "age": 30}
